f45bcf6d91789a232bed10b47095c87e1a81411c
Adriano Ernesto <angolawap@gmail.com>
2024-01-26 02:39:22 +0100

    Inicio

---
+++ pack.js
@@ -0,0 +1,28 @@
+const packager = require('electron-packager');
+
+var options = {
+    'arch': 'ia32',
+    'platform': 'win32',
+    'dir': './',
+    'app-copyright': 'Paulo Galdo',
+    'app-version': '2.0.5',
+    'asar': true,
+    'icon': './imagens/logo.ico',
+    'name': 'TierraDesktop',
+    'ignore': ['./releases', './.git'],
+    'out': './releases',
+    'overwrite': true,
+    'prune': true,
+    'version': '1.3.2',
+    'version-string':{
+        'CompanyName': 'Paulo Galdo',
+        'FileDescription': 'Tierra de colores', /*This is what display windows on task manager, shortcut and process*/
+        'OriginalFilename': 'TierraDesktop',
+        'ProductName': 'Tierra de colores',
+        'InternalName': 'TierraDesktop'
+    }
+};
+packager(options, function done_callback(err, appPaths) {
+    console.log(err);
+    console.log(appPaths);
+});
---
+++ src/main.ts
@@ -0,0 +1,188 @@
+import { app, BrowserWindow, ipcMain, dialog, Menu, shell } from 'electron';
+import path from 'path';
+
+// eslint-disable-next-line @typescript-eslint/no-var-requires
+const Store = require('electron-store');
+const store = new Store();
+
+// Handle creating/removing shortcuts on Windows when installing/uninstalling.
+if (require('electron-squirrel-startup')) {
+  app.quit();
+}
+
+const createWindow = () => {
+  // Create the browser window.
+  new BrowserWindow({
+    width: 800,
+    height: 600,
+    webPreferences: {
+      preload: path.join(__dirname, 'preload.js'),
+    },
+    show: false
+  });
+
+  let mainWindowWa: BrowserWindow = null;
+
+  const slug = store.get("slug")
+
+  if(slug === undefined){
+
+    mainWindowWa = new BrowserWindow({
+      width: 600,
+      height: 700,
+      webPreferences: {
+        preload: path.join(__dirname, 'preload.js'),
+        nodeIntegration: true,
+        contextIsolation: false,
+        nodeIntegrationInWorker: true
+      },
+      resizable: false,
+      show: false,
+    });
+
+    mainWindowWa.maximize();
+
+    mainWindowWa.show();
+
+    mainWindowWa.setMenu(null);
+    //mainWindowWa.webContents.openDevTools();
+    mainWindowWa.loadFile("setEscola.html");
+
+  } else {
+    startApp();
+  }
+
+  // Event handler for asynchronous incoming messages
+  ipcMain.on('box_error', (event, arg) => {
+    dialog.showErrorBox("Ocorreu um erro", arg);
+  })
+
+
+  // Event handler for asynchronous incoming messages
+  ipcMain.on('save_url', (event, arg) => {
+
+    if(mainWindowWa){
+      mainWindowWa.close();
+    }
+    store.set("slug", arg);
+    startApp();
+  })
+
+  // Open the DevTools.
+  //mainWindow.webContents.openDevTools();
+};
+
+const startApp = ()=>{
+
+  const slug = store.get("slug")
+
+  const mainWindow = new BrowserWindow({
+    width: 800,
+    height: 600,
+    webPreferences: {
+      preload: path.join(__dirname, 'preload.js'),
+    },
+    show: false
+  });
+
+  const menu = Menu.buildFromTemplate([
+    {
+      label: 'Ajuda',
+      click: ()=> {
+        shell.openExternal("https://ajuda.escola.ao")
+      }
+    },
+    {
+      label: "Web",
+      click: ()=> {
+        shell.openExternal("https://"+slug+".escola.ao");
+      }
+    },
+    {
+      label: "Site principal",
+      click: ()=> {
+        shell.openExternal("https://escola.ao");
+      }
+    },
+    {
+      label: "Contactos",
+      click: ()=> {
+        shell.openExternal("https://escola.ao/contactos");
+      }
+    },
+    {
+      label: "Sobre",
+      click: ()=> {
+        const windowSobre = new BrowserWindow({
+          width: 400,
+          height: 400,
+          webPreferences: {
+            preload: path.join(__dirname, 'preload.js'),
+          },
+          maximizable: false,
+          minimizable: false,
+          center: true,
+        });
+
+        windowSobre.setTitle("Algo")
+        windowSobre.setMenu(null);
+        windowSobre.loadFile("sobre.html");
+      }
+    }
+  ]);
+
+  Menu.setApplicationMenu(menu);
+
+  mainWindow.loadURL("https://"+slug+".escola.ao")
+
+  const loadingWindow = new BrowserWindow({
+    width: 512,
+    height: 384,
+    resizable: false,
+    transparent: true,
+    frame: false,
+  })
+
+  loadingWindow.loadFile('loading.html')
+
+  mainWindow.webContents.on('did-finish-load', () => {
+    if (loadingWindow) loadingWindow.close();
+    mainWindow.maximize();
+    mainWindow.show();
+  });
+
+  mainWindow.webContents.on('did-fail-load', () => {
+    dialog.showErrorBox("Ocorreu um erro", "Não conseguimos carregar!")
+    mainWindow.close();
+  });
+
+  mainWindow.on("close", ()=> {
+    app.quit();
+  });
+}
+
+// This method will be called when Electron has finished
+// initialization and is ready to create browser windows.
+// Some APIs can only be used after this event occurs.
+app.on('ready', createWindow);
+// Quit when all windows are closed, except on macOS. There, it's common
+// for applications and their menu bar to stay active until the user quits
+// explicitly with Cmd + Q.
+app.on('window-all-closed', () => {
+  if (process.platform !== 'darwin') {
+    app.quit();
+  }
+});
+
+app.on('activate', () => {
+  // On OS X it's common to re-create a window in the app when the
+  // dock icon is clicked and there are no other windows open.
+
+  if (BrowserWindow.getAllWindows().length === 0) {
+    createWindow();
+  }
+
+});
+
+// In this file you can include the rest of your app's specific main process
+// code. You can also put them in separate files and import them here.
